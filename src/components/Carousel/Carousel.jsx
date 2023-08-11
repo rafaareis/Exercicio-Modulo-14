01eb5e739af3a30f3a6dc07abb231d66194b5332
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-08-11 16:33:07 -0300

    Commit 11082023-1632: Concluindo o carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,5 +1,6 @@
+
 import { useEffect, useState } from "react"
-import styles from './Carousel.module.css'
+import styles from "./Carousel.module.css"
 
 const Carousel = () => {
     const dbProductsURL = "https://json-db-modulo-14.vercel.app/products"
@@ -14,19 +15,66 @@ const Carousel = () => {
         })
     }, [])
 
+    let items = document.querySelectorAll('.carousel .carousel-item')
+
+    items.forEach((el) => {
+        const minPerSlide = 6
+        let next = el.nextElementSibling
+        for (var i=1; i<minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    })
+
     return (
         <>
-            {albunsArr.map((albuns) => {
-                return (
-                    <div key={albuns.id}>
-                        <img 
-                            className={styles.capaDoDisco} 
-                            src={albuns.capaDoDisco} 
-                            alt={`Álbum de ${albuns.artista} - ${albuns.nomeDoDisco}`} 
-                        />
+            <div className="container text-center my-3">
+                <div className="row mx-auto my-auto justify-content-center">
+                    <div id="recipeCarousel" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-inner" role="listbox">
+                            {albunsArr.map((albuns) => {
+                                if(albuns.id === 1) {
+                                    return (
+                                        <div key={albuns.id} className="carousel-item active">
+                                            <div className="col-md-3">
+                                                <div className="card">
+                                                    <div className="card-img">
+                                                        <img className={`img-fluid ${styles.capaDoDisco}` } src={albuns.capaDoDisco} alt={`Capa de ${albuns.artista} - ${albuns.nomeDoDisco}`} />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                } else {
+                                    return (
+                                        <div key={albuns.id} className="carousel-item">
+                                            <div className="col-md-3">
+                                                <div className="card">
+                                                    <div className="card-img">
+                                                        <img className={`img-fluid ${styles.capaDoDisco}`} src={albuns.capaDoDisco} alt={`Capa de ${albuns.artista} - ${albuns.nomeDoDisco}`} />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            })}
+                            <a className="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            </a>
+                            <a className="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            </a>
+                        </div>
                     </div>
-                )
-            })}
+                </div>
+            </div>
+
         </>
     )
 }
