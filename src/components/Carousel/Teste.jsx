a141d0195497d9d043e56346d128c04811cbbc48
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-08-12 22:15:33 -0300

    Commit 12082023-2215

--- src/components/Carousel/Teste.jsx
+++ src/components/Carousel/Teste.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react"
+import Container from "react-bootstrap/Container"
+import Carousel from "react-bootstrap/Carousel"
 import Card from "react-bootstrap/Card"
 import styles from './Carousel.module.css'
 
 
-const Carousel = () => {
+
+const Teste = () => {
     const dbProductsURL = "https://json-db-modulo-14.vercel.app/products"
     const [albunsArr, setAlbunsArr] = useState([])
 
@@ -16,23 +19,45 @@ const Carousel = () => {
         })
     }, [])
 
+    let items = document.querySelectorAll('.carousel .carousel-item')
+
+    items.forEach((el) => {
+        const minPerSlide = 6
+        let next = el.nextElementSibling
+        for (var i=1; i<minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    })
+
     return (
         <>
-            <div className="container">
-                {albunsArr.map((albuns) => {
-                    return (
-                        <Card key={albuns.id}>
-                            <Card.Img 
-                                src={albuns.capaDoDisco}
-                                className={`${styles.capaDoDisco}`}
-                            />
-                        </Card>
-                    )
-                })}
-            </div>
+            <Container>
+                <Carousel>
+                    {albunsArr.map((albuns) => {
+                        return (
+                            <Carousel.Item key={albuns.id}>
+                                <Card 
+                                className={`col-md-3`}
+                                >
+                                    <Card.Img 
+                                        src={albuns.capaDoDisco}
+                                        className={`${styles.capaDoDisco}`}
+                                    />
+                                </Card>
+                            </Carousel.Item>
+                        )
+                    })}
+                </Carousel>
+            </Container>
 
         </>
     )
 }
 
-export default Carousel
+export default Teste
