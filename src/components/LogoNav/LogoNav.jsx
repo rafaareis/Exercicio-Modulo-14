88547781196110e08875aec463066f0b02b53874
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-08-16 16:52:46 -0300

    Commit 16082023-1652

--- src/components/LogoNav/LogoNav.jsx
+++ src/components/LogoNav/LogoNav.jsx
@@ -1,5 +1,5 @@
 import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
+import Row from "react-bootstrap/Row" 
 import styles from './LogoNav.module.css'
 
 const LogoNav = () => {
@@ -8,33 +8,44 @@ const LogoNav = () => {
         <>
             <header id="cabecalho" className="navbar navbar-expand-lg sticky-top">
                 <Container>
-                    <div className={styles.logotipo}>
-                        <h1>Mus<span className={styles.logo_span}>!</span>c Store</h1>
-                        <h5 className="navbar-brand">A loja do fã de música</h5>
-                    </div>
-                    <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu-navegacao">
-                        <span className="navbar-toggler-icon">
-                        </span>
-                    </button>
-                    <Nav className="navbar-collapse collapse justify-content-end" id="menu-navegacao">
-                        <ul className="nav nav-pills d-block d-md-flex">
-                            <li className="nav-item fw-bold" id={styles.navLi}>
-                                <a className="nav-link"  href="#sobre">
-                                    Sobre a Loja                        
-                                </a>
-                            </li>
-                            <li className="nav-item fw-bold" id={styles.navLi}>
-                                <a className="nav-link"  href="#produtos">
-                                    Produtos
-                                </a>
-                            </li>
-                            <li className="nav-item fw-bold" id={styles.navLi}>
-                                <a className="nav-link"  href="#contatos">
-                                    Contato
-                                </a>
-                            </li>
-                        </ul>
-                    </Nav>
+                    <Row>
+                        <div className={styles.logotipo}>
+                            <h1>Mus<span className={styles.logo_span}>!</span>c Store</h1>
+                            <h5 className="navbar-brand">A loja do fã de música</h5>
+                        </div>
+                        <button 
+                            className="navbar-toggler"
+                            data-bs-toggle="collapse" 
+                            data-bs-target="#menu-navegacao"
+                            aria-controls="menu-navegacao" 
+                            aria-expanded="false" 
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon">
+                            </span>
+                        </button>
+                    </Row>
+                    <Row>
+                        <nav className="navbar-collapse collapse justify-content-end" id="menu-navegacao">
+                            <ul className="nav nav-pills d-block d-md-flex">
+                                <li className="nav-item fw-bold" id={styles.navLi}>
+                                    <a className="nav-link"  href="#sobre">
+                                        Sobre a Loja                        
+                                    </a>
+                                </li>
+                                <li className="nav-item fw-bold" id={styles.navLi}>
+                                    <a className="nav-link"  href="#produtos">
+                                        Produtos
+                                    </a>
+                                </li>
+                                <li className="nav-item fw-bold" id={styles.navLi}>
+                                    <a className="nav-link"  href="#contatos">
+                                        Contato
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </Row>
                 </Container>
             </header>
         </>
