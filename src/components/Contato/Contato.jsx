cb81fe708cb8259dedf351d7210119bddcb4fe08
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-08-25 16:27:43 -0300

    Commit 25082023-1627

--- src/components/Contato/Contato.jsx
+++ src/components/Contato/Contato.jsx
@@ -46,43 +46,43 @@ const Contato = () => {
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="telefone" className="form-label fw-bold">Celular:</label>
-                                    <input type="tel" id="telefone" className="form-control" />
+                                    <input type="tel" id="telefone" className="form-control" required />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="cpf" className="form-label fw-bold">CPF:</label>
-                                    <input type="tel" id="cpf" className="form-control" />
+                                    <input type="tel" id="cpf" className="form-control" required />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="cep" className="form-label fw-bold">CEP:</label>
-                                    <input type="tel" id="cep" className="form-control" onBlur={checkCEP} />
+                                    <input type="tel" id="cep" className="form-control" required onBlur={checkCEP} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="endereco" className="form-label fw-bold">Endereço:</label>
-                                    <input type="text" id="endereco" className="form-control" value={cepAddress.logradouro} />
+                                    <input type="text" id="endereco" className="form-control" required value={cepAddress.logradouro} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="enderecoNum" className="form-label fw-bold">Número do Logradouro:</label>
-                                    <input type="tel" id="enderecoNum" className="form-control" />
+                                    <input type="tel" id="enderecoNum" className="form-control" required />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="bairro" className="form-label fw-bold">Bairro:</label>
-                                    <input type="text" id="bairro" className="form-control" value={cepAddress.bairro} />
+                                    <input type="text" id="bairro" className="form-control" required value={cepAddress.bairro} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="complemento" className="form-label fw-bold">Complemento:</label>
-                                    <input type="text" id="complemento" className="form-control" value={cepAddress.complemento} />
+                                    <input type="text" id="complemento" className="form-control" required value={cepAddress.complemento} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="cidade" className="form-label fw-bold">Cidade:</label>
-                                    <input type="text" id="cidade" className="form-control" value={cepAddress.localidade} />
+                                    <input type="text" id="cidade" className="form-control" required value={cepAddress.localidade} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="estado" className="form-label fw-bold">Estado:</label>
-                                    <input type="text" id="estado" className="form-control" value={cepAddress.uf} />
+                                    <input type="text" id="estado" className="form-control" required value={cepAddress.uf} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="item" className="form-label fw-bold">Item de interesse:</label>
-                                    <input type="text" id="item" className="form-control" />
+                                    <input type="text" id="item" className="form-control" required />
                                 </div>
                                 <button type="submit" className={`btn fw-bold ${styles.btn_contacts} `}>Enviar</button>
                             </form>
